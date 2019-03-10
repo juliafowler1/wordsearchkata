3ce27ca96bd1b8b020739a6b2f64c1712d8aaad1
Julia Fowler <juliafowler@live.com>
2019-03-10 17:18:01 -0400

    added stubs for tests, implemented tests for initialization and horizontal search

--- test/test.js
+++ test/test.js
@@ -1,17 +1,41 @@
 const assert = require("assert");
 const Puzzle = require("../src/Puzzle");
 const data = require("./data");
+const path = require("path");
 
-describe("puzzle tests", function() {
-  describe("puzzle class instantiation", function() {
-    // test puzzle and words properties
-    // test for invalid input
+describe("puzzle tests", () => {
+  describe("puzzle class instantiation", () => {
+    it("should initialize puzzle and words properties", () => {
+      const { puzzle, words } = data.puzzle1;
+      const filePath = path.resolve(__dirname, "puzzle1.txt");
+      const p = new Puzzle();
+      p.initializePuzzle(filePath)
+        .then(() => {
+          assert.deepEqual(p.puzzle, puzzle);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    });
+    it("should throw an error if puzzle is invalid", () => {});
   });
-  describe("word searching", function() {
-    // test for horizontal
-    // test for vertical
-    // test for diagonal descending
-    // test for diagonal ascending
-    // test for backwards
+  describe("word searching", () => {
+    it("should search horizontally", () => {
+      const { puzzle, words } = data.puzzle1;
+      const filePath = path.resolve(__dirname, "puzzle1.txt");
+      const p = new Puzzle();
+      p.initializePuzzle(filePath)
+        .then(() => {
+          p.searchHorizontally("GREASE");
+          console.log(p.wordCoordinates);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    });
+    it("should search vertically", () => {});
+    it("should search diagonally descending", () => {});
+    it("should search diagonally ascending", () => {});
+    it("should search backwards", () => {});
   });
 });
